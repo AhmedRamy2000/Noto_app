10f366c72856fa0cde406931699991ccef6ec24c
AhmedRamy2000 <125711699+AhmedRamy2000@users.noreply.github.com>
2023-08-29 05:41:22 +0300

    create custom note item

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class NotoApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(brightness: Brightness.dark),
+      theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
       home: NotesView(),
     );
   }
--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:notesapp/views/widgets/custom_app_bar.dart';
+import 'package:notesapp/widgets/NoteItem.dart';
+import 'package:notesapp/widgets/custom_app_bar.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -12,6 +13,7 @@ class NotesViewBody extends StatelessWidget {
         children: [
           SizedBox(height: 50),
           CustomAppBar(),
+          NoteItem(),
         ],
       ),
     );
